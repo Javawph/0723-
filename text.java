ebb845d4e56dbc3e374d9d4de9126d058b65a8ed
offcn-java <myqq@qq.com>
2018-12-05 13:38:36 +0800

    xinde

---
+++ text.java
@@ -0,0 +1,11 @@
+package cn.com.webxml;
+
+/**
+ * @author 迈克杰克逊
+ * @date 2018/12/4-21:11
+ */
+public class text {
+    public static void main(String[] args) {
+           System.out.println("=========");
+    }
+}
